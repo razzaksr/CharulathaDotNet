ae60bacaf040fa0eb47021ab1ddea52a3b8ff508
razzaksr <razzaksr@gmail.com>
2023-07-28 12:39:52 +0530

    Single inheritance

---
+++ OOPs/Inher.cs
@@ -0,0 +1,51 @@
+using encap;
+namespace Heritance{
+    // derivedclass:baseclass
+    class DebitCard:KYC{
+        public int pin{set;get;}
+        public DebitCard(int pin){
+            this.pin=pin;
+        }
+
+        public bool isValid(int curPin){
+            if(this.pin==curPin)
+                return true;
+            else
+                return false;
+        }
+
+        public static DebitCard operator >> (DebitCard debitCard,int amount){
+            Console.WriteLine("Enter the pin to verify ");
+            int myPin=Convert.ToInt32(Console.ReadLine());
+            if(debitCard.isValid(myPin)==true){
+                debitCard.accBal+=amount;
+                Console.WriteLine(amount+" has deposited");
+            }
+            else{
+                Console.WriteLine("Unauthoriser");
+            }
+            return debitCard;
+        }
+
+        public static DebitCard operator -- (DebitCard debitCard){
+            Console.WriteLine("Enter the pin ");
+            int myPin=Convert.ToInt32(Console.ReadLine());
+            if(debitCard.isValid(myPin)){
+                Console.WriteLine("Enter the amount to debit");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                if(amount<=debitCard.accBal){
+                    debitCard.accBal-=amount;
+                    Console.WriteLine(amount+" withdrawl has success");
+                }
+                else{
+                    Console.WriteLine("Invalid transaction amount");
+                }
+            }
+            else{
+                Console.WriteLine("unauthoriser");
+            }
+            return debitCard;
+        }
+
+    }
+}
--- OOPs/Program.cs
+++ OOPs/Program.cs
@@ -1,19 +1,9 @@
-﻿// using Overloading;
+﻿using Heritance;
 
-// CompileTime compile=new CompileTime();
-// String[] myTechs=new String[]{"Spring","DJango","Bootstrap","Node","Express","Hibernate","Flask"};
-// // compile.filter(myTechs);
-// // compile.filter(myTechs,"Go");
-// // compile.filter("Kotlin",myTechs);
-// // int myPoint=-1;
-// // compile.filter(myTechs,"Kotlin",out myPoint);
-// // Console.WriteLine(myPoint);
+DebitCard debitCard=new DebitCard(4590);
+debitCard.accNum=876545678;
+debitCard.accBal=920043.2;
+debitCard.holder="Razak Mohamed";
 
-// compile.arrange(myTechs);
-// compile.showArray(myTechs);
-// compile.arrange(myTechs,"Node",myTechs.Length-1);
-
-using encap;
-KYC kyc1=new KYC(656789876,9800.3,"Razak");
-kyc1=kyc1<<10000;
-Console.WriteLine(kyc1.ToString());
+//debitCard=debitCard>>4000;
+--debitCard;
